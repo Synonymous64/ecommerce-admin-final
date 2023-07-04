6d0b726ce7037b92a7b6f275b7c34e4f722b13dc
Synonymous64 <prajwalurkude007@gmail.com>
2023-07-04 19:46:40 +0530

    cloudinary config

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -53,7 +53,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="i8wl3zyq">
         {({ open }) => {
           const onClick = () => {
             open();
